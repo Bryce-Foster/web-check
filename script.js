eb7014dc9ef6a082084820db2546660fc379744d
TheHydra778 <54971206+TheHydra778@users.noreply.github.com>
2022-02-01 06:34:43 -0600

    confusing

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ const express = require('express');
 const app = express();
 const { readfile, readFile } = require('fs');
 
-app.use(express.static(_dirname + '/public/'));
 
 app.get('/', (request, response) => {
-    readFile('./public/views/index.html', 'utf8', (err, html) => {
+    readFile('./index.html', 'utf8', (err, html) => {
         if (err) {
             response.status(500).send("sorry, out of order")
         }
